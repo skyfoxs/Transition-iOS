ff978adfd80d6a384d5e825307eae4d9e5dea96e
Supakit Thanadittagorn <supakit@Supakits-MacBook-Pro.local>
2017-06-15 21:19:00 +0700

    Initial Commit

---
+++ Transition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transition
+//
+//  Created by Supakit Thanadittagorn on 6/15/17.
+//  Copyright © 2017 Supakit Thanadittagorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
